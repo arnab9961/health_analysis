622b237111e5d18dcd621a6c70e203da948619cd
arnab <75444354+arnab9961@users.noreply.github.com>
2025-04-15 11:47:36 +0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -190,4 +190,4 @@ async def analyze_health_data(health_data: HealthData):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
